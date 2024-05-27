64df13437703aa6866a1cc813fe27337deaa44ba
StephenCurry7 <93304806+StephenCurry7@users.noreply.github.com>
2024-05-27 15:10:36 +0800

    Update and rename CombineModel.py to Combine_DeepFM_Model.py

--- Combine_DeepFM_Model.py
+++ Combine_DeepFM_Model.py
@@ -2,11 +2,11 @@ import os
 import gc
 from sklearn.metrics import log_loss, mean_squared_error
 import torch
-from torch import nn #from tensorflow import keras
-from torch.nn import Module #from tensorflow.python.keras import Model
-from deepctr_torch.models import xDeepFM, DeepFM, WDL, DCN, AutoInt #from deepctr.models import xDeepFM, DeepFM, WDL, DCN, AutoInt
+from torch import nn 
+from torch.nn import Module 
+from deepctr_torch.models import xDeepFM, DeepFM, WDL, DCN, AutoInt 
 from deepctr_torch.models.basemodel import BaseModel
-import copy #from tensorflow.python.keras.models import clone_model
+import copy 
 from deepctr_torch.inputs import combined_dnn_input
 import numpy as np
 import copy
@@ -18,21 +18,20 @@ class CombinedModel(BaseModel):
             base_part = copy.deepcopy(base_model)
             base_part.load_state_dict(base_model.state_dict())
             for param in base_part.parameters():
-                param.requires_grad = False #冻结base_model，不训练这一部分（同论文，先训练interaction部分，之后再fine_tune整个集成模型）
+                param.requires_grad = False 
 
             inter_part = copy.deepcopy(inter_model)
             inter_part.load_state_dict(inter_model.state_dict())
             
             self.base_part = base_part
             self.inter_part = inter_part
-            self.feature_names_num = feature_names_num#原特征个数
+            self.feature_names_num = feature_names_num
             self.task = task
             
         def forward(self, input): 
-            base_input = input[:,:self.feature_names_num] #base模型的输入
-            inter_input = input[:,self.feature_names_num:] #inter模型的输入
+            base_input = input[:,:self.feature_names_num] 
+            inter_input = input[:,self.feature_names_num:] 
             
-            #求base_input的倒数第二层输出(根据deepfm的代码写)
             sparse_embedding_list, dense_value_list = self.base_part.input_from_feature_columns(base_input, self.base_part.dnn_feature_columns,self.base_part.embedding_dict)
             logit = self.base_part.linear_model(base_input)
 
@@ -45,11 +44,10 @@ class CombinedModel(BaseModel):
                     sparse_embedding_list, dense_value_list)
                 dnn_output = self.base_part.dnn(dnn_input)
                 dnn_logit = self.base_part.dnn_linear(dnn_output)
-                logit += dnn_logit #此处得到的logit为deepfm倒数第二层的输出
+                logit += dnn_logit 
             
             base_output = logit.squeeze()
             
-            #求base_input的倒数第二层输出
             sparse_embedding_list, dense_value_list = self.inter_part.input_from_feature_columns(inter_input, self.inter_part.dnn_feature_columns,self.inter_part.embedding_dict)
             logit = self.inter_part.linear_model(inter_input)
 
@@ -62,11 +60,11 @@ class CombinedModel(BaseModel):
                     sparse_embedding_list, dense_value_list)
                 dnn_output = self.inter_part.dnn(dnn_input)
                 dnn_logit = self.inter_part.dnn_linear(dnn_output)
-                logit += dnn_logit #此处得到的logit为deepfm倒数第二层的输出
+                logit += dnn_logit 
             
             inter_output = logit.squeeze()
 
-            x = torch.add(base_output, inter_output) #拼接两个模型的输出
+            x = torch.add(base_output, inter_output) 
             if self.task == "binary":
                 x = torch.sigmoid(x)
             final_output = x.view(-1, 1) #模型的预测概率
